ad0d5141cf9a991bfb3b8f83f90c9fa11a7321d9
dlagnlwo <105468233+dlagnlwo@users.noreply.github.com>
2023-09-03 20:13:09 +0900

    Add files via upload

--- AlbumController.java
+++ AlbumController.java
@@ -9,8 +9,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AlbumController {
 
     @GetMapping
-    public String album(){
-        return "/album/album";
+    public String album_song(){
+        return "/album/album_song";
+    }
+
+    @GetMapping("/detail")
+    public String album_detail(){
+        return "/album/album_detail";
+    }
+
+    @GetMapping("/comment")
+    public String album_comment(){
+        return "/album/album_comment";
     }
 
 }
